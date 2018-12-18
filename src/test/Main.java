85172a2f61b85c73183a0b12887b329d94be78b9
BerberLee <saberazizi66@yahoo.com>
2018-12-19 01:49:48 +0330

    Saber Test 3

--- src/test/Main.java
+++ src/test/Main.java
@@ -1,13 +1,19 @@
 package test;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
-        for (int i=1;i<=10;i++){
-            for (int j = 1; j <=10 ; j++) {
-                System.out.print(i+"x"+j+"="+(i*j)+"\t");
+        for (int i = 1; i <= 10; i++) {
+            for (int j = 1; j <= 10; j++) {
+                System.out.print(i + "x" + j + "=" + (i * j) + "\t");
             }
             System.out.println();
         }
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter Your Name :  ");
+        String name = scanner.next();
+        System.out.println("Welcome "+name);
     }
 }
